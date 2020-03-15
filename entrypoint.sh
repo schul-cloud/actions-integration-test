bf185c557bcecb03f8fe4a4843e99e8bcf7596eb
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-03-15 11:27:38 +0100

    get correct branch name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo $GITHUB_REF
-export BRANCH_NAME=${GITHUB_REF##*/}
+export BRANCH_NAME=${GITHUB_REF#refs/heads/}
 echo $BRANCH_NAME
 
 curl "https://raw.githubusercontent.com/schul-cloud/integration-tests/develop/scripts/ci/fetch.github.sh" | bash
